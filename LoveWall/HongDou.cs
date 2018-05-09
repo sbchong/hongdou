b961900b9bc6932f3a54484daafd8d58e0efa43a
CC Liu <Y-Y-Liu@hotmail.com>
2018-05-09 23:08:57 +0800

    加入连接服务器，登录选项

---
+++ LoveWall/HongDou.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Net.Sockets;
+
+namespace LoveWall
+{
+    public class HongDou
+    {
+        public class ClientControl
+        {
+            //private Socket clientSocket;
+            public Socket clientSocket;
+
+            string msgrec;
+
+            public ClientControl()
+            {
+                clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            }
+
+            public bool Connect(string ip, int port)
+            {
+                try
+                {
+                    clientSocket.Connect(ip, port);
+                    clientSocket.Send(Encoding.UTF8.GetBytes("haha hgx12345"));
+                    clientSocket.Send(Encoding.UTF8.GetBytes("haha 1"));
+                    clientSocket.Send(Encoding.UTF8.GetBytes("lalalala"));
+                }
+                catch
+                {
+                    return true;
+                }
+                //Thread threadReceive = new Thread();
+                //threadReceive.IsBackground = true;
+                //threadReceive.Start();
+                
+                return false;
+            }
+
+        }
+    }
+}
--- LoveWall/MainPage.xaml.cs
+++ LoveWall/MainPage.xaml.cs
@@ -2,16 +2,21 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net.Sockets;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
+using System.Threading;
+using System.Threading.Tasks;
 using Windows.UI.Xaml.Controls.Primitives;
 using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using System.Text.RegularExpressions;
 
 // https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x804 上介绍了“空白页”项模板
 
@@ -22,9 +27,72 @@ namespace LoveWall
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        HongDou hongdou = new HongDou();
+        private Socket clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        string msgrec = "";
+
         public MainPage()
         {
             this.InitializeComponent();
+ 
+        }
+
+        public string Receive()
+        {
+            byte[] msg = new byte[20 * 20];
+            int msgLen = clientSocket.Receive(msg);
+            msgrec = Encoding.UTF8.GetString(msg, 0, msgLen);
+            //Receive();
+            //await Receive();
+            return msgrec;
+        }
+
+        public string Receive1()
+        {
+
+            byte[] msg = new byte[20 * 20];
+            int msgLen = clientSocket.Receive(msg);
+            // msgrec = Encoding.Unicode.GetString(msg, 0, msgLen);
+            msgrec = Encoding.UTF8.GetString(msg, 0, msgLen);
+            msgrec = Regex.Unescape(msgrec);
+
+            return msgrec;
+        }
+
+        private async void PlayerButton_ClickAsync(object sender, RoutedEventArgs e)
+        {
+            /*************
+            MediaElement media = new MediaElement();
+            var synth = new Windows.Media.SpeechSynthesis.SpeechSynthesizer();
+            Windows.Media.SpeechSynthesis.SpeechSynthesisStream stream =
+                await synth.SynthesizeTextToStreamAsync("游，gay");
+            media.SetSource(stream, stream.ContentType);
+            media.Play();
+            ****************/
+            clientSocket.Send(Encoding.UTF8.GetBytes("/login"));
+            Receive();
+        }
+
+        private void MenuButton_Click(object sender, RoutedEventArgs e)
+        {
+            clientSocket.Connect("45.63.91.170", 20566);
+            //Receive();
+            //Thread threadReceive = new Thread(Receive);
+            //threadReceive.IsBackground = true;
+            //threadReceive.Start();
+
+            //clientSocket.Send(Encoding.UTF8.GetBytes("/login"));
+            //clientSocket.Send(Encoding.UTF8.GetBytes("haha hgx12345"));
+            //clientSocket.Send(Encoding.UTF8.GetBytes("/newMessage"));
+            //clientSocket.Send(Encoding.UTF8.GetBytes("haha 1"));
+            //clientSocket.Send(Encoding.UTF8.GetBytes("lalalala"));
+
+        }
+
+        private void LoginButton_Click(object sender, RoutedEventArgs e)
+        {
+            clientSocket.Send(Encoding.UTF8.GetBytes("haha hgx12345"));
+            Receive();
         }
     }
 }
